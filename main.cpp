af0bc53b218479b9e0aa58a52fcc5059deadc47e
kedraxton <44153177+kedraxton@users.noreply.github.com>
2018-10-30 23:34:08 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,87 @@
+// CodeHunter.cpp : Defines the entry point for the console application.
+//
+
+//added the cstdlib and took out "stdafx.h"
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main()
+{
+    string textToAnalyze;
+    //changed this to class and not int, foo is used as a placeholder for a value
+    //that can change
+    class foo;
+    int vowels = 0;
+    int consonants = 0;
+    int digits = 0;
+    int spaces = 0;
+    int lengthOfStringSubmittedForAnalysis = 0;
+    int unknownCharacters = 0;
+    int checkSum = 0;
+    //changed this from int to class, same reason as foo above
+    class bar;
+    
+    cout << "Welcome to the CIA code Hunter Program!" << endl;
+    cout << "Please type in text to analyze: " << endl;
+    getline(cin, textToAnalyze);
+    
+    for (int i = 0; i < textToAnalyze.length(); ++i)
+    {
+        if (textToAnalyze[i] == 'a' || textToAnalyze[i] == 'e' || textToAnalyze[i] == 'i' ||
+            textToAnalyze[i] == 'o' || textToAnalyze[i] == 'u' || textToAnalyze[i] == 'A' ||
+            textToAnalyze[i] == 'E' || textToAnalyze[i] == 'I' || textToAnalyze[i] == 'O' ||
+            textToAnalyze[i] == 'U')
+        {
+            //changed this from -- to ++ because we need to add the number of
+            //vowels in the statement
+            ++vowels;
+        }
+        else if ((textToAnalyze[i] >= 'a'&& textToAnalyze[i] <= 'z') || (textToAnalyze[i] >= 'A'&& textToAnalyze[i] <= 'Z'))
+        {
+            ++consonants;
+        }
+        else if (textToAnalyze[i] >= '0' && textToAnalyze[i] <= '9')
+        {
+            ++digits;
+        }
+        else if (textToAnalyze[i] == ' ')
+        {
+            ++spaces;
+        }
+        else
+        {
+            ++unknownCharacters;
+        }
+    }
+    
+    lengthOfStringSubmittedForAnalysis = textToAnalyze.length();
+    checkSum = unknownCharacters + vowels + consonants + digits + spaces;
+    
+    cout << "Vowels: " << vowels << endl;
+    cout << "Consonants: " << consonants << endl;
+    cout << "Digits: " << digits << endl;
+    cout << "White spaces: " << spaces << endl;
+    cout << "Length of string submitted for analysis: " << lengthOfStringSubmittedForAnalysis << endl;
+    cout << "Number of characters CodeHunter could not identify: " << unknownCharacters << endl;
+    cout << "Checksum: " << checkSum << endl;
+    
+    if (checkSum == lengthOfStringSubmittedForAnalysis)
+    {
+        cout << "This program self checking has found this Analysis to be valid." << endl;
+    }
+    
+    else
+    {
+        cout << "WARNING! *** This program self checking has found this Analysis to be invalid! Do not use this data!" << endl;
+    }
+    
+    //system pause only works for windows....
+    cin.get();
+    
+    return 0;
+}
